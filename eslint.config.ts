93b81512cf853a3fdad2429caecf926130284735
Jepil Yun <jepilyun@gmail.com>
2025-09-21 16:38:19 +0900

    Updated

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,9 +3,9 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {ignores: ["**/node_modules/", "./dist/"]}
+  { ignores: ["**/node_modules/", "./dist/"] },
 ];
