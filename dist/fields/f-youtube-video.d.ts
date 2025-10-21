1493df6cb5e9051a50306ad90fca1910246f0ba1
Jepil Yun <jepilyun@gmail.com>
2025-10-22 00:36:26 +0900

    Updated

--- dist/fields/f-youtube-video.d.ts
+++ dist/fields/f-youtube-video.d.ts
@@ -71,7 +71,7 @@ export declare const F_YOUTUBE_VIDEO: {
         max_length: number;
         placeholder: string;
     };
-    ai_summary: {
+    info_country: {
         id: string;
         label: string;
         is_required: boolean;
@@ -79,7 +79,7 @@ export declare const F_YOUTUBE_VIDEO: {
         placeholder: string;
         default_value: string;
     };
-    main_topics: {
+    info_city: {
         id: string;
         label: string;
         is_required: boolean;
@@ -87,7 +87,7 @@ export declare const F_YOUTUBE_VIDEO: {
         placeholder: string;
         default_value: string;
     };
-    key_points: {
+    info_district: {
         id: string;
         label: string;
         is_required: boolean;
@@ -95,7 +95,87 @@ export declare const F_YOUTUBE_VIDEO: {
         placeholder: string;
         default_value: string;
     };
-    keywords: {
+    info_neighborhood: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_landmark: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_category: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_name: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_special_tag: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_influencer: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_season: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_time_of_day: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_activity_type: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_reservation_required: {
+        id: string;
+        label: string;
+        is_required: boolean;
+        max_length: number;
+        placeholder: string;
+        default_value: string;
+    };
+    info_travel_tips: {
         id: string;
         label: string;
         is_required: boolean;
